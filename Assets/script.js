b2a61e43ddb8b8e1470b50321bde1ac9ecee4632
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-20 22:51:59 -0600

    final commit, having fetch issues

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,29 @@
 var searchButton = document.getElementById("button-addon1");
 var APIkey = "93b1a8aba548ebcefe09b6f2e82b258f";
-var city;
+var city = ""
 
-function getApi() {
-    
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
-    
-function searchCity() {
-    var searchCity = searchButton
 
-    fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data) 
+// searchButton.addEventListener("click", getWeather);
+function smSearch() {
+    var city = document.getElementById("smTopic").value;
+    
+    var weatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIkey;
+    
+    function getWeather() {
+        fetch(weatherURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            
+        });
         
-        for (var i = 0; i < data.length; i++) {
-            
-            
-        }
-    });
-    
-}    
+        console.log();
+    }
     
 }
-    
-searchButton.addEventListener('click', getApi);
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Forcasting-the-Weather
+------------------------------------------------
+
+## Description 
+___________________________
+
+This page allows users to search for a city and find the weather in that city for 5 days.
+The user will be able to see 5 days of forecasts.
+
+## Screenshot
+
+___________________________________
+
+
+![The Calander](Forcasting-the-Weather\Assets\Screenshot.JPG)
