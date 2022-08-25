040b995fc2de9ca3cdcdf8ed1616858987eb556d
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-24 18:49:02 -0600

    slowly getting there, made more JS changes

--- script.js
+++ script.js
@@ -41,15 +41,21 @@ function getForecast(cityForecast) {
     .then(function (data) {
       console.log(data);
 
-      for (i = 0; i <= 5; i++) {
-        var tempData = data.data.[i].temp
-        var windSpeed = data.data.[i].wind_spd
-        var uv = data.data.[i].uv
-        var precip = data.data.[i].precip
+      for (i = 0; i < 5; i++) {
+        var tempData = data.data[i].temp;
+        var windSpeed = data.data[i].wind_spd;
+        var uv = data.data[i].uv;
+        var precip = data.data[i].precip;
+        var humidity = data.data[i].rh;
+        var weatherIcon = data.data.weather[i].icon;
+        console.log(tempData);
 
-        document.getElementById(`temp${i + 1}`).innerHTML = tempData;
-        
-        
+        document.getElementById("temp" + (i + 1)).innerHTML = tempData;
+        document.getElementById("wind" + (i + 1)).innerHTML = windSpeed;
+        document.getElementById("uv" + (i + 1)).innerHTML = uv;
+        document.getElementById("precip" + (i + 1)).innerHTML = precip;
+        document.getElementById("rh" + (i + 1)).innerHTML = humidity;
+        document.getElementById("icon" + (i + 1)).innerHTML = weatherIcon;
       }
     });
 }
@@ -57,4 +63,3 @@ function getForecast(cityForecast) {
 
 //var cardIconUrl =
 // "https://www.weatherbit.io/static/img/icons/" + cardIcon + ".png";
-// data.data.
