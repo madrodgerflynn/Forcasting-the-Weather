ef6362e6591ce69b22346d5c06fe0806915195a3
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-26 20:46:39 -0600

    fixed the JS to work with current day

--- Assets/README.md
+++ Assets/README.md
@@ -1,15 +1,34 @@
 # Forcasting-the-Weather
-------------------------------------------------
 
-## Description 
-___________________________
+---
+
+## Description
+
+---
 
 This page allows users to search for a city and find the weather in that city for 5 days.
 The user will be able to see 5 days of forecasts.
 
+## User Story
+
+As a traveler
+I want to see the weather outlook for multiple cities
+so that I can plan a trip accordingly
+
+---
+
+## Instalation
+
+---
+
+The insallation is straightforward. Just open the link in your browser. :)
+
 ## Screenshot
 
-___________________________________
-
+---
 
 ![The Calander](Screenshot.JPG)
+
+---
+
+## Deployed Link
--- script.js
+++ script.js
@@ -3,10 +3,14 @@ var APIkey = "93b1a8aba548ebcefe09b6f2e82b258f";
 var apiKey = "a9c9a0d72993004daad86c302cb27400";
 var bitKey = "2d834009bc27453f896fb7da27cc02b1";
 var city = "";
-
+var searchHistory = [];
 // searchButton.addEventListener("click", getWeather);
 function smSearch() {
   var city = document.getElementById("smTopic").value;
+  searchHistory.unshift(city);
+  console.log(searchHistory);
+
+  localStorage.setItem("city", JSON.stringify(city));
 
   var searchCity =
     "https://api.weatherbit.io/v2.0/current?city=" +
@@ -23,13 +27,30 @@ function smSearch() {
   getForecast(cityForecast);
 }
 
+function init() {
+  var storedCities = JSON.parse(localStorage.getItem(searchHistory));
+
+  if (storedCities !== null) {
+    searchHistory = storedCities;
+  }
+
+  smSearch();
+}
+
 function getCity(searchCity) {
   fetch(searchCity)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log(data.data[0].temp);
+      document.getElementById("currentTemp").textContent = data.data[0].temp;
+      document.getElementById("currentWindSpeed").textContent =
+        data.data[0].wind_spd;
+      document.getElementById("currentUv").textContent = data.data[0].uv;
+      document.getElementById("currentPrecip").textContent =
+        data.data[0].precip;
+      document.getElementById("currentRh").textContent = data.data[0].rh;
     });
 }
 
@@ -52,7 +73,7 @@ function getForecast(cityForecast) {
           "https://www.weatherbit.io/static/img/icons/" +
           data.data[i].weather.icon +
           ".png";
-        console.log(cardIcon);
+        // console.log(cardIcon);
         console.log(dateTime);
 
         console.log(tempData);
@@ -62,7 +83,7 @@ function getForecast(cityForecast) {
         document.getElementById("uv" + (i + 1)).innerHTML = uv;
         document.getElementById("precip" + (i + 1)).innerHTML = precip;
         document.getElementById("rh" + (i + 1)).innerHTML = humidity;
-        // document.getElementById("date" + (i + 1)).innerHTML = dateTime;
+        document.getElementById("date" + (i + 1)).innerHTML = dateTime;
         document.getElementById("image" + (i + 1)).src = cardIcon;
       }
     });
